1df902334f613bf5bfa40153faf9a355b84a309c
Joseph Chung <chungss@digi.com.my>
2016-08-24 12:23:10 +0800

    test comment

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 <a id="repository-link" href="https://github.com/kayrules/yosegame/blob/master/README.md#readme">readme</a>
 <a id="contact-me-link" href="contact.php">Contact Me</a>
 <a id="ping-challenge-link" href="/ping">Ping</a>
+
+TEST
