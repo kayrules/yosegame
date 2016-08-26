7dc599de0d024f9b5abc1ff7e23d2f8c456c0414
Joseph Chung <chungss@digi.com.my>
2016-08-26 12:24:01 +0800

    20160826

--- astroport/index.php
+++ astroport/index.php
@@ -1,30 +1,62 @@
 <?php
-$ship1 = "Ship 1";
 
-if (isset($_GET["ship"])) {
-  $ship1 = $_GET["ship"];
+$class1 = "free";
+$class2 = "free";
+$class3 = "free";
+
+$ship = @$_GET["ship"];
+
+if (!empty($ship)) {
+  $ship1  = $ship;
+  $class1 = "occupied";
 }
 
 ?>
 
 <html>
-<header>
+<head>
 <title>Astroport-wegi</title>
-</header>
+
+<style type="text/css">
+  .free {
+    color: white;
+    background-color: green;
+  }
+  .occupied {
+    color: white;
+    background-color: red;
+  }
+  .hidden {
+    display: none;
+  }
+</style>
+
+<script type="text/javascript">
+  function onKeyDown() {
+    document.getElementById("info").className = "hidden";
+  }
+</script>
+
+</head>
 <body>
 
 <h1 id="astroport-name">Astroport-wegi</h1>
 
-<h2 id="gate-1">Gate 1: <span id="ship-1"><?php echo $ship1; ?></span></h2>
-<h2 id="gate-2">Gate 2: <span id="ship-2">Ship 2</span></h2>
-<h2 id="gate-3">Gate 3: <span id="ship-3">Ship 3</span></h2>
+<h2 id="gate-1" class="<?php echo $class1; ?>">Gate 1: <span id="ship-1"><?php echo ($ship1) ? $ship1 : "None"; ?></span></h2>
+<h2 id="gate-2" class="<?php echo $class2; ?>">Gate 2: <span id="ship-2"><?php echo ($ship2) ? $ship2 : "None"; ?></span></h2>
+<h2 id="gate-3" class="<?php echo $class3; ?>">Gate 3: <span id="ship-3"><?php echo ($ship2) ? $ship3 : "None"; ?></span></h2>
 
-<form action="." method="get">
+<h3 id="info" class="<?php echo ($ship) ? "anything" : "hidden"; ?>">Info: <?php echo ($ship) ? "$ship has docked at Gate 1" : "None"; ?></h3>
 
-Ship <input id="ship" name="ship" type="text" />
-<button id="dock" name="dock" type="button">Dock</button>
+<form name="dock-form" id="dock-form" action="." method="get">
+
+Ship Name:
+<input id="ship" name="ship" type="text" value="<?php echo $ship; ?>" onkeydown="onKeyDown();" />
+<button id="dock" name="dock" type="submit"  value="1">Dock</button>
+<button id="reset" name="reset" type="reset" value="reset" onclick="window.location='./'">Reset</button>
 
 </form>
+
 
 
 </body>
